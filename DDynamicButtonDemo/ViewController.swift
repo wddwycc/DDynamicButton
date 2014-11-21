9b2e76db1e2c09a8b2f65a1988093db928d93e21
wddwycc <wddwyss@gmail.com>
2014-11-21 15:07:13 +0800

    Initial Commit

---
+++ DDynamicButtonDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DDynamicButtonDemo
+//
+//  Created by 端 闻 on 21/11/14.
+//  Copyright (c) 2014年 monk-studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DDynamicButtonDemoTests/DDynamicButtonDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DDynamicButtonDemoTests.swift
+//  DDynamicButtonDemoTests
+//
+//  Created by 端 闻 on 21/11/14.
+//  Copyright (c) 2014年 monk-studio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DDynamicButtonDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
